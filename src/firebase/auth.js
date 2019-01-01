05d157062bcc0ca5238fee2109b32d8989b930e6
alexa griffin <alexa.griffin42@gmail.com>
2018-12-31 20:57:43 -0500

    heck

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,10 +1,17 @@
+import firebase from "firebase"
 import app from "./firebase"
 
 const auth = app.auth()
 
-const createUser = uid => {
+const createUserKey = user => {
+  console.log("pretend I am making the db entry for this user: " + uid)
   //TODO make db creation scripts for new users
 }
 
+const createGoogleUser = () => {
+  const provider = new firebase.auth.GoogleAuthProvider()
+  return auth.signInWithPopup(provider)
+}
+
 export default auth
-export { createUser }
+export { createUserKey, createGoogleUser }
--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import auth from "../firebase/auth"
+import auth, { createGoogleUser, createUserKey } from "../firebase/auth"
 
 export default class Signup extends Component {
 
@@ -8,8 +8,11 @@ export default class Signup extends Component {
     e.preventDefault()
 
     try {
-      const user = await auth.createUserWithEmailAndPassword("alexa.griffin42@gmail.com", "password123");
-      this.props.history.push("/")
+      const user = createGoogleUser().then(res => {
+        console.log(res)
+        createUserKey(res.user.uid)
+        this.props.history.push("/")
+      });
     } catch (error) {
       alert(error)
     }
