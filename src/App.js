ab47103854e9936ebf494210cc919ef272df48f2
alexa griffin <alexa.griffin42@gmail.com>
2018-12-31 20:12:48 -0500

    heck

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>HIIIIII</h1>
+        <h1>{ (process.env.REACT_APP_HECK) }</h1>
       </div>
     )
   }
