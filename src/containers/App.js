7fbee82eef791793d891fb3540b644d73869d482
alexa griffin <alexa.griffin42@gmail.com>
2018-12-31 20:35:13 -0500

    basic routing, todo

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom"
+
+import Signup from "./Signup"
+import Login from "./Login"
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Router className="app">
-          <Route path="/heck" render={ e => <h1>hello from heck</h1> }/>
+          <>
+            <Switch>
+              <Route exact path="/signup" component={ Signup }/>
+              <Route exact path="/login" component={ Login }/>
+
+              <Redirect to="/login" />
+            </Switch>
+          </>
         </Router>
       </div>
     )
