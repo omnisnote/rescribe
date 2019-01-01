99f91b9f57d2642cef3e31884e54ff51af68b287
alexa griffin <alexa.griffin42@gmail.com>
2018-12-31 21:46:22 -0500

    heck

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,5 +1,6 @@
 import firebase from "firebase"
 import app from "./firebase"
+import auth from "./auth"
 
 const db = app.firestore()
 
@@ -8,7 +9,6 @@ db.settings({
 })
 
 const createUser = user => {
-  console.log("hi")
   db.collection("users").doc(user.uid).set({
     displayName: user.displayName,
     uid: user.uid,
@@ -16,5 +16,14 @@ const createUser = user => {
   })
 }
 
+// const getUserData = ref => {
+//   if(!auth.currentUser) return null
+//   console.log(db.collection("users").doc(auth.currentUser.uid).get())
+// }
+
+const getUserRef = () => {
+  return db.collection("users").doc(auth.currentUser.uid)
+}
+
 export default db
-export { createUser }
+export { createUser, getUserRef }
--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react'
 
-import { createGoogleUser, createUserKey } from "../firebase/auth"
-import { createUser } from "../firebase/db"
+import auth, { createGoogleUser } from "../firebase/auth"
+import { createUser, getUserRef } from "../firebase/db"
 
 export default class Signup extends Component {
 
-  handleSignup = async  e => {
-    e.preventDefault()
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      user: {}
+    }
+  }
+
+  handleSignup = async e => {
+    e.preventDefault()
     try {
       const user = createGoogleUser().then(res => {
-        console.log(res)
         createUser(res.user)
-      });
+      })
     } catch (error) {
       alert(error)
     }
+  }
+
+  componentDidMount() {
+    this.authObservable = auth.onAuthStateChanged(user => {
+      const ref = getUserRef()
+      this.userObservable = ref.onSnapshot(snapshot => {
+        this.setState({
+          user: snapshot.data()
+        })
+      })
+    })
+    
+  }
+
+  componentWillUnmount() {
+    this.authObservable = null
+    this.userObservable = null
   }
 
   render() {
     return (
       <>
         <h1>Signup</h1>
+        <p>{ JSON.stringify(this.state.user) }</p>
         <button onClick={ e => this.handleSignup(e) }>test</button>
       </>
     )
