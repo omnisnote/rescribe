c657f8684e040697b458d3fa8554eee63fe781e4
alexa griffin <alexa.griffin42@gmail.com>
2019-01-01 21:35:07 -0500

    less

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -446,6 +446,22 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
+            // CLIENT DEFINED RULES
+            {
+              test: /\.less$/,
+              use: [{
+                loader: 'style-loader',
+              }, {
+                loader: 'css-loader', // translates CSS into CommonJS
+              }, {
+                loader: 'less-loader', // compiles Less to CSS
+                options: {
+                  javascriptEnabled: true,
+                },
+              }],
+              sideEffects: true,
+            },
+            // END CLIENT DEFINED RULES
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
