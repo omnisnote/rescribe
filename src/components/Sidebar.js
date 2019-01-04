00eaff4cbae79dfdb81ed452d06c8de269a18e26
alexa griffin <alexa.griffin42@gmail.com>
2019-01-03 19:14:26 -0500

    heck

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom"
+import { Icon } from "antd"
+
 
 export default class Sidebar extends Component {
   render() {
     return (
       <div className="sidebar">
-
+        <ul>
+          <li>
+            <Link to="/notes">
+              <Icon type="bars" style={{ fontSize: "32px", color: "rgba(255,255,255, 0.85)" }}/>
+            </Link>
+          </li>
+          <li>
+            <Link to="/settings">
+              <Icon type="setting" style={{ fontSize: "32px", color: "rgba(255,255,255, 0.85)" }}/>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              <Icon type="question" style={{ fontSize: "32px", color: "rgba(255,255,255, 0.85)" }}/>
+            </Link>
+          </li>
+        </ul>
+        
+        
+        
       </div>
     )
   }
