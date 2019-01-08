128ac27d0eabaadb6dd3fa09af94a2dceff0a925
alexa griffin <alexa.griffin42@gmail.com>
2019-01-08 09:50:11 -0500

    heck

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class Loading extends Component {
   render() {
     return (
-      <div className="loader"></div>
+      <div className="loader">
+        <div className="planet"></div>
+      </div>
     )
   }
 
