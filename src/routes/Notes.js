1dd30cf86435058489590f9886d54ace2841f702
alexa griffin <alexa.griffin42@gmail.com>
2019-01-12 19:54:50 -0500

    god i hate my life

--- src/routes/Notes.js
+++ src/routes/Notes.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom"
 
+import MaterialIcon from 'material-icons-react'
+
 import UserContext from "../context"
 
 import { transformToArr, getUserRef, createNote } from "../firebase/db"
@@ -23,17 +25,21 @@ export default class Notes extends Component {
     return (
       <UserContext.Consumer>{ context => (
         <>
-        <Sidebar color="rgba(255,255,255,0.85)" size={ 36 }/>
-        <div className="notes">
-          <NewNote onCreate={ (ref, res) => this.create(ref, res) } />
-          { context.notes ? transformToArr(context.notes).map((note, i) => (
-            <Link to={"/note/" + note.uid} key={i} className="note">
-              <div>
-                { note.title || "untitled note" }
-              </div>
-            </Link>
-          )) : <Loading /> }
+          <Sidebar color="rgba(255,255,255,0.85)" size={ 36 }/>
+          <div className="notes">
+            <NewNote onCreate={ (ref, res) => this.create(ref, res) } />
+            { context.notes ? transformToArr(context.notes).map((note, i) => (
+              <Link to={"/note/" + note.uid} key={i} className="note">
+                <div>
+                  { note.title || "untitled note" }
+                </div>
+              </Link>
+            )) : <Loading /> }
+
           </div>   
+          <button className="add" onClick={ e => this }>
+            <MaterialIcon icon="add" size={32} />
+          </button>
         </>
       )}</UserContext.Consumer>
     )
