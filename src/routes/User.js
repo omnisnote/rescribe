3a69138e1898a92395a89097804a0bd29064113c
alexa griffin <alexa.griffin42@gmail.com>
2019-01-27 20:58:48 -0500

    god today has been awful

--- src/routes/User.js
+++ src/routes/User.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import db, { getUserRef } from "../firebase/db"
 import auth from "../firebase/auth"
 
+import Sidebar from "../containers/Sidebar"
+
 export default class User extends Component {
   constructor(props) {
     super(props)
@@ -15,21 +17,15 @@ export default class User extends Component {
     auth.signOut()
   }
 
-  push() {
-    getUserRef().update({
-      [("random-" + Math.random()).replace(/\./g, "-")]: "heck",
-      dog: Math.random()
-    })
-  }
-
   render() {
     return (
       <>
-        <h1>user</h1>
-        <button onClick={ e => this.signOut(e) }>sign out</button>
-        <button onClick={ e => this.push(e) }>add thing to test</button>
+        <Sidebar />
+        <div>
+          <h1>user</h1>
+          <button onClick={ e => this.signOut(e) }>sign out</button>
+        </div>
       </>
     )
   }
-
 }
